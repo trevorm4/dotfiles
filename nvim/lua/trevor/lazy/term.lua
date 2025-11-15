737778fe23a733fdcd0770cd20e19d6b8c6f394d
Trevor <trevor4swe@gmail.com>
2025-11-14 16:37:22 -0800

    More term hotkey work

--- nvim/lua/trevor/lazy/term.lua
+++ nvim/lua/trevor/lazy/term.lua
@@ -1,28 +1,46 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
-
 function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)  -- alternative
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts) -- alternative
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 return {
 	"waiting-for-dev/ergoterm.nvim",
 	version = "*",
 	config = function()
-		require("ergoterm").setup()
+		local Terminal = require("ergoterm")
+		Terminal.setup()
+
+		local function prompt_for_name_and_open_terminal(layout)
+			local name = vim.fn.input("Enter terminal name: ")
+			if name and name ~= "" then
+				Terminal:new({ name = string.format("%s (%s)", name, layout), layout = layout }):focus(layout)
+			else
+				print("No terminal name provided.")
+			end
+		end
 		-- Terminal creation with different layouts
-		map("n", "<leader>cb", ":TermNew layout=below<CR>", opts) -- Split below
-		map("n", "<leader>cr", ":TermNew layout=right<CR>", opts) -- Vertical split
-		map("n", "<leader>cf", ":TermNew layout=float<CR>", opts) -- Floating window
-		map("n", "<leader>ct", ":TermNew layout=tab<CR>", opts) -- New tab
-		--        map("t", "<esc>", [[<C-\><C-n>]], opts)
+		map("n", "<leader>cb", function()
+			prompt_for_name_and_open_terminal("below")
+		end, opts)
+		map("n", "<leader>cr", function()
+			prompt_for_name_and_open_terminal("right")
+		end, opts)
+		map("n", "<leader>cf", function()
+			prompt_for_name_and_open_terminal("float")
+		end, opts)
+		map("n", "<leader>ct", function()
+			prompt_for_name_and_open_terminal("tab")
+		end, opts)
+		map("n", "<leader>cs", ":TermSelect<CR>", opts) -- select terminal
+		map("n", "<leader>cl", ":TermSelect!<CR>", opts) -- select last terminal
 	end,
 }
