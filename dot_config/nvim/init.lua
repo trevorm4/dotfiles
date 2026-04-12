1f2a87762bfe606b0f4b6dbad2df4bee9e2200bd
Trevor Moyer <trevormoyer@google.com>
2026-04-12 00:58:26 +0000

    Attempt to fix

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -20,10 +20,53 @@ require("lazy").setup({
       priority = 1000,
       config = function()
         require("trevor")
-        vim.cmd.colorscheme("sonokai")
       end,
     },
     { import = "trevor.lazy" },
   },
   change_detection = { notify = false },
 })
+
+-- Parity with nvim-base/lua/trevor/init.lua
+local augroup = vim.api.nvim_create_augroup
+local group = augroup("Trevor", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+
+function R(name)
+  require("plenary.reload").reload_module(name)
+end
+
+vim.filetype.add({
+  extension = {
+    templ = "templ",
+  },
+})
+
+autocmd("TextYankPost", {
+  group = yank_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 40,
+    })
+  end,
+})
+
+autocmd({ "BufWritePre" }, {
+  group = group,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+vim.o.autoread = true
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
