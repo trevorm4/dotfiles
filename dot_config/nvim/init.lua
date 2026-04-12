643c441bf27566a9e145c27b5ace9e21445a6137
Trevor Moyer <trevormoyer@google.com>
2026-04-12 00:21:35 +0000

    Switch to plugin for neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,30 @@
-require("trevor")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    {
+      "trevorm4/nvim-base",
+      name = "trevor",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require("trevor")
+        vim.cmd.colorscheme("sonokai")
+      end,
+    },
+    { import = "trevor.lazy" },
+    { import = "trevor.custom" },
+  },
+  change_detection = { notify = false },
+})
