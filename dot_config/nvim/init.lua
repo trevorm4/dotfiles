9667ec4c130c613a5e58c7259c6754539e66c8ee
Trevor Moyer <trevormoyer@google.com>
2026-04-12 01:29:30 +0000

    Take two

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -22,51 +25,13 @@ require("lazy").setup({
         require("trevor")
       end,
     },
+    -- Import specs from the plugin
     { import = "trevor.lazy" },
+    -- Import specs from this machine's local configuration (e.g. code-layout, lazyjj)
+    { import = "trevor.machine" },
   },
   change_detection = { notify = false },
 })
 
--- Parity with nvim-base/lua/trevor/init.lua
-local augroup = vim.api.nvim_create_augroup
-local group = augroup("Trevor", {})
-
-local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup("HighlightYank", {})
-
-function R(name)
-  require("plenary.reload").reload_module(name)
-end
-
-vim.filetype.add({
-  extension = {
-    templ = "templ",
-  },
-})
-
-autocmd("TextYankPost", {
-  group = yank_group,
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch",
-      timeout = 40,
-    })
-  end,
-})
-
-autocmd({ "BufWritePre" }, {
-  group = group,
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
-})
-
-vim.o.autoread = true
-
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = { "*" },
-})
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- Apply colorscheme after lazy setup
+vim.cmd.colorscheme("sonokai")
